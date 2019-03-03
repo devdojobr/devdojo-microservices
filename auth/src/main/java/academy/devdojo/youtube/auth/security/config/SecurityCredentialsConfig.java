a4e2a89a68747bbb9f82be14d43d560939ee220c
williamsuane <williamsuane@gmail.com>
2019-03-03 21:17:53 +0100

    authentication service part 1

---
+++ auth/src/main/java/academy/devdojo/youtube/auth/security/config/SecurityCredentialsConfig.java
@@ -0,0 +1,55 @@
+package academy.devdojo.youtube.auth.security.config;
+
+import academy.devdojo.youtube.core.property.JwtConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+/**
+ * @author William Suane
+ */
+@EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final JwtConfiguration jwtConfiguration;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilter(new UsernamePasswordAuthenticationFilter())
+                .authorizeRequests()
+                .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
+                .antMatchers("/course/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ core/src/main/java/academy/devdojo/youtube/core/property/JwtConfiguration.java
@@ -0,0 +1,32 @@
+package academy.devdojo.youtube.core.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author William Suane
+ */
+@Configuration
+@ConfigurationProperties(prefix = "jwt.config")
+@Getter
+@Setter
+@ToString
+public class JwtConfiguration {
+    private String loginUrl = "/login/**";
+    @NestedConfigurationProperty
+    private Header header = new Header();
+    private int expiration = 3600;
+    private String privateKey = "qxBEEQv7E8aviX1KUcdOiF5ve5COUPAr";
+    private String type = "encrypted";
+
+    @Getter
+    @Setter
+    public static class Header {
+        private String name = "Authorization";
+        private String prefix = "Bearer ";
+    }
+}
